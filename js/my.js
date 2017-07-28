dbe7d378370c371cf5aacc5174c943d2dfba86c1
Cabpy <wenxiangxu1994@gmail.com>
2017-07-27 23:42:53 -0400

    mobile

--- js/my.js
+++ js/my.js
@@ -17,3 +17,16 @@ function form_reset(){
 	$('#email').val('');
 	$('#password').val('');
 }
+
+
+$('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+      onOpen: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is opened
+      onClose: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is closed
+    }
+);
+
+ 
